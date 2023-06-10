ef6177060a954f8567a1a6947df7e65f678ceab7
Berkay öztunç <mailberkayoztunc@gmail.com>
2023-06-10 21:07:14 +0000

    update buffer files

--- dapp/PurplePiggy/vite.config.ts
+++ dapp/PurplePiggy/vite.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base : '/purple-piggy',
   define: {
-    'process.env': {}
+    'process.env': {},
+    'window.global': {},
+
   },
   build: {
     rollupOptions: {
-      external: [
-        'project-serum/anchor'
-      ],
+      plugins: [
+        nodePolyfills({ crypto: true }),
+        NodeGlobalsPolyfillPlugin({ buffer: true }),
+      ]
     },
   },
 })
